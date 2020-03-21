d0df40164b32e67b8653a651f6a8aace5ee109c4
Atul Sharma <2012atulsharma@gmail.com>
2020-03-21 11:39:36 +0530

    Railway

---
+++ Railway Platforms/MinRailwayPlatforms.cpp
@@ -0,0 +1,50 @@
+// Program to find minimum number of platforms 
+// required on a railway station 
+#include<iostream> 
+#include<algorithm> 
+
+using namespace std; 
+
+// Returns minimum number of platforms reqquired 
+int findPlatform(int arr[], int dep[], int n) 
+{ 
+// Sort arrival and departure arrays 
+sort(arr, arr+n); 
+sort(dep, dep+n); 
+
+// plat_needed indicates number of platforms needed at a time 
+int plat_needed = 1, result = 1; 
+int i = 1, j = 0; 
+
+// Similar to merge in merge sort to process all events in sorted order 
+while (i < n && j < n) 
+{ 
+	// If next event in sorted order is arrival,increment count of platforms needed
+	
+	if (arr[i] <= dep[j]) 
+	{ 
+		plat_needed++; 
+		i++;  
+		if (plat_needed > result) 
+			result = plat_needed; 
+	} 
+
+		else
+	{ 
+		plat_needed--; 
+		j++; 
+	} 
+} 
+
+return result; 
+} 
+
+int main() 
+{ 
+	int arr[] = {900, 940, 950, 1100,1110, 1500, 1800}; 
+	int dep[] = {910, 1200, 1120, 1130, 1900, 2000}; 
+	int n = sizeof(arr)/sizeof(arr[0]); 
+	cout << "\nMinimum Number of Platforms Required =  "
+		<< findPlatform(arr, dep, n); 
+	return 0; 
+} 
